882a00b6a16d4e1e34d9372e6a2c74fffd59646b
Ella Brady <112596525+ellaroseb@users.noreply.github.com>
2023-11-16 23:38:51 -0600

    code added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 function init(){
 //add your javascrip between these two lines of code
- 
 
+  //get div element
+  var button = document.getElementById('entrybutton');
+  
+  //buttonClicked function
+  function buttonClicked(){
+	var input = document.getElementById('entryinput');
+	document.getElementById('textoutput').innerHTML = input.value;
+	alert("Ella Brady: " + input.value);
+  });
 
+  button.addEventListener('click', buttonClicked);
+  });
 
-
-
-
-
-window.addEventListener('load', init);
+  window.addEventListener('load', init);
